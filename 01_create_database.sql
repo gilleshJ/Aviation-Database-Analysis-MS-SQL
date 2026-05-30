f9c441af2bfb1e74bdbe5350081dfa2613b4b443
gilleshJ <85328027+gilleshJ@users.noreply.github.com>
2026-05-30 19:28:21 -0400

    Create air_cargo database and initialize script

---
+++ 01_create_database.sql
@@ -0,0 +1,16 @@
+-- ============================================================
+-- Air Cargo Database Analysis
+-- Script 01: Create Database
+-- Author: gilleshJ
+-- Database: MS SQL Server 2019
+-- ============================================================
+
+-- Create the air_cargo database
+CREATE DATABASE air_cargo;
+GO
+
+-- Switch to the new database
+USE air_cargo;
+GO
+
+PRINT 'Database air_cargo created successfully.';
